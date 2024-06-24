bf943da58a6cdd6b8bd76b8a6150cfd156f3c4bd
marcinstenka <mastenka@wp.pl>
2024-06-24 23:12:44 +0200

    feat: initial files for backend

---
+++ backend/src/main/java/stenka/marcin/backend/StenkowaApplication.java
@@ -0,0 +1,12 @@
+package stenka.marcin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StenkowaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StenkowaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/stenka/marcin/backend/StenkowaApplicationTests.java
@@ -0,0 +1,13 @@
+package stenka.marcin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StenkowaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
