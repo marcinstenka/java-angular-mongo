d00c5fac627759ccceee8a7ba53d170346895c86
marcinstenka <mastenka@wp.pl>
2024-06-26 18:24:24 +0200

    feat: todo findAll method in controller & repository

--- backend/src/main/java/stenka/marcin/backend/todo/controller/TodoController.java
+++ backend/src/main/java/stenka/marcin/backend/todo/controller/TodoController.java
@@ -1,12 +1,27 @@
 package stenka.marcin.backend.todo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import stenka.marcin.backend.todo.model.Todo;
+import stenka.marcin.backend.todo.repository.TodoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/todo")
 public class TodoController {
 
+    private TodoRepository todoRepository;
 
+    public TodoController(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    @GetMapping("")
+    List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
 }
 
--- backend/src/main/java/stenka/marcin/backend/todo/repository/TodoRepository.java
+++ backend/src/main/java/stenka/marcin/backend/todo/repository/TodoRepository.java
@@ -1,14 +1,25 @@
 package stenka.marcin.backend.todo.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import stenka.marcin.backend.todo.model.Todo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public class TodoRepository {
 
-    private List<Todo> todos = new ArrayList<Todo>();
+    private final List<Todo> todos = new ArrayList<Todo>();
 
+    public List<Todo> findAll() {
+        return todos;
+    }
+
+    @PostConstruct
+    private void init() {
+        todos.add(new Todo(123, "Test nazwy 1", "Test opisu 1", "red", new Date()));
+        todos.add(new Todo(321, "Test nazwy 2", "Test opisu 2", "black", new Date()));
+    }
 }
